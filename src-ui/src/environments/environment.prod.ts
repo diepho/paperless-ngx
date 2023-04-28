0485d0d174a66899c70a451d0fdba6f311e4e34f
Diep Ho <diepho@gmail.com>
2023-04-28 17:40:03 +0700

    init

--- src-ui/src/environments/environment.prod.ts
+++ src-ui/src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
   production: true,
   apiBaseUrl: document.baseURI + 'api/',
   apiVersion: '2',
-  appTitle: 'Paperless-ngx',
+  appTitle: 'Smart vOCR',
   version: '1.14.2-dev',
   webSocketHost: window.location.host,
   webSocketProtocol: window.location.protocol == 'https:' ? 'wss:' : 'ws:',
